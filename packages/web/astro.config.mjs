54fe85e478c78599cba4ba6d08f6a0011795dcea
lhlhlhl <1765069638@qq.com>
2026-03-14 14:07:32 +0800

    update

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
     configSchema(),
     solidJs(),
     starlight({
-      title: "OpenCode",
+      title: "Amis",
       defaultLocale: "root",
       locales: {
         root: {
